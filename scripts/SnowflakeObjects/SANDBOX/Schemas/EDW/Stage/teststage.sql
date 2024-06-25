72a8ca306b1677766a60cf82cd5c01d378dcdc50
Tharun-Suncor <151141226+Tharun-Suncor@users.noreply.github.com>
2024-06-25 23:18:52 +0530

    Update teststage.sql

--- scripts/SnowflakeObjects/SANDBOX/Schemas/EDW/Stage/teststage.sql
+++ scripts/SnowflakeObjects/SANDBOX/Schemas/EDW/Stage/teststage.sql
@@ -1,5 +1,5 @@
 CREATE STAGE IF NOT EXISTS SNOWPARK.pythonstage 
 	DIRECTORY = ( ENABLE = true );
 
-PUT 'file://helper_class.py' @SNOWPARK.pythonstage AUTO_COMPRESS=FALSE;
+PUT 'file://~/helper_class.py' @SNOWPARK.pythonstage AUTO_COMPRESS=FALSE;
 
