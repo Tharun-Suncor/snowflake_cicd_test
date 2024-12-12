ba3d7389bb70efedcf7b638365ad72b7544f8a04
Tharun-Suncor <151141226+Tharun-Suncor@users.noreply.github.com>
2024-12-12 11:45:02 +0530

    new

---
+++ SILVER/SnowflakeObjects/SANDBOX/Schemas/EDW/Stage/teststage.sql
@@ -0,0 +1,5 @@
+CREATE STAGE IF NOT EXISTS EDW.pythonstage 
+	DIRECTORY = ( ENABLE = true );
+
+PUT 'file://~helper_class.py' @EDW.pythonstage AUTO_COMPRESS=FALSE;
+
--- scripts/SnowflakeObjects/SANDBOX/Schemas/EDW/Stage/teststage.sql
+++
@@ -1,5 +0,0 @@
-CREATE STAGE IF NOT EXISTS SNOWPARK.pythonstage 
-	DIRECTORY = ( ENABLE = true );
-
-PUT 'file://~helper_class.py' @SNOWPARK.pythonstage AUTO_COMPRESS=FALSE;
-
